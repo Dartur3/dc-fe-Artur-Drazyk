91160aab51538007a3b9dd0201bcc8c12c30a6b6
Artur Drażyk <artudra568@student.polsl.pl>
2022-08-22 20:35:29 +0200

    added favourites tab

--- src/views/FavouritesView.vue
+++ src/views/FavouritesView.vue
@@ -1,5 +1,69 @@
 <template>
   <div>
-    <h1>This is a favourites page</h1>
+    <SearchBar />
+    <div v-if="isFavourites()">
+      <p v-if="error">Something went wrong...</p>
+      <p v-else-if="loading">Loading...</p>
+      <div v-else-if="result && result.charactersByIds" v-for="character in result.charactersByIds" :key="character.id">
+        <img :src="`${character.image}`">
+        <p class="inline">{{ character.id }} {{ character.name }}</p>
+        <button type="button" class="inline" v-on:click="removeFavourite(character.id)">X</button>
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+
+import gql from 'graphql-tag'
+import { defineComponent, reactive } from 'vue';
+import { useQuery } from '@vue/apollo-composable';
+import SearchBar from '../components/SearchBar.vue';
+import { getFavourites, deleteFavourite } from './HomeView.vue';
+
+const FAVOURITES_QUERY = gql`
+    query CharactersByIds ($ids: [ID!]!) {
+      charactersByIds (ids: $ids) {
+        id
+        name
+        image
+      }
+    }
+  `
+export default defineComponent({
+
+  name: 'FavouritesView',
+
+  components: {
+    SearchBar
+  },
+
+  setup() {
+
+    const variables = reactive({
+      ids: getFavourites()
+    })
+
+    const { result, loading, error } = useQuery(FAVOURITES_QUERY, variables)
+
+    return {
+      result,
+      loading,
+      error
+    }
+
+  },
+
+  methods: {
+
+    isFavourites() {
+      if (getFavourites().length > 0) return true
+      else return false
+    },
+
+    removeFavourite(id: number) {
+      deleteFavourite(id)
+    }
+  }
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,22 @@
 
   <button v-if="page > 1" type="button" class="droptxt" v-on:click="setPage(--page)">-</button>
   <button v-else type="button" class="droptxt">-</button>
-  <button v-if="resultNames && resultNames.characters.info && page < resultNames.characters.info.count / 20" type="button" class="droptxt" v-on:click="setPage(++page)">+</button>
+  <button v-if="resultNames && resultNames.characters.info && page < resultNames.characters.info.count / 20"
+    type="button" class="droptxt" v-on:click="setPage(++page)">+</button>
   <button v-else type="button" class="droptxt">+</button>
 
-  <SearchBar v-on:pass-search-bar-data="passSearchBarData" v-on:initialize-input="setName(id.name), setNameId(id.nameId), setEpisodeId(id.episode), setPage(page)"/>
+  <SearchBar v-on:pass-search-bar-data="passSearchBarData"
+    v-on:initialize-input="setName(id.name), setId(id.nameId), setEpisodeId(id.episode), setPage(page)" />
+  <p class="inline" v-for="favourite in getFavourites()" :key="favourite">{{ favourite }}</p>
 
   <div v-if="buttonClicked.name">
-    <!-- <p class="inline" v-for="favourite in getFavourites()" :key="favourite">{{ favourite }}</p> -->
     <p v-if="errorNames">Something went wrong...</p>
     <p v-else-if="loadingNames">Loading...</p>
-    <div v-else-if="resultNames && resultNames.characters.results" v-for="character in resultNames.characters.results"
-      :key="character.id">
+    <div v-else-if="resultNames && resultNames.characters.results" v-for="character in resultNames.characters.results" :key="character.id">
       <img :src="`${character.image}`">
       <p class="inline">{{ character.id }} {{ character.name }}</p>
-      <button class="inline" type="button" v-on:click="addFavourite(character.id)">&#9829;</button>
+      <button v-if="getFavourites().indexOf(character.id) === -1" class="inline" type="button" v-on:click="addFavourite(character.id)">&#9829;</button>
+      <button v-else type="button" class="inline" v-on:click="deleteFavourite(character.id)">X</button>
     </div>
   </div>
 
@@ -25,6 +27,8 @@
     <div v-else-if="resultId && resultId.character">
       <img :src="`${resultId.character.image}`">
       <p class="inline">{{ resultId.character.id }} {{ resultId.character.name }}</p>
+      <button v-if="getFavourites().indexOf(resultId.character.id) === -1" class="inline" type="button" v-on:click="addFavourite(resultId.character.id)">&#9829;</button>
+      <button v-else type="button" class="inline" v-on:click="deleteFavourite(resultId.character.id)">X</button>
     </div>
   </div>
 
@@ -35,6 +39,8 @@
       v-for="character in resultEpisode.episode.characters" :key="character.id">
       <img :src="`${character.image}`">
       <p class="inline">{{ character.id }} {{ character.name }}</p>
+      <button v-if="getFavourites().indexOf(character.id) === -1" class="inline" type="button" v-on:click="addFavourite(character.id)">&#9829;</button>
+      <button v-else type="button" class="inline" v-on:click="deleteFavourite(character.id)">X</button>
     </div>
   </div>
 </template>
@@ -47,63 +53,79 @@ import { useQuery } from '@vue/apollo-composable';
 import SearchBar from '../components/SearchBar.vue';
 
 const NAMES_QUERY = gql`
-  query Names ($page: Int!, $name: String!) {
-    characters (page: $page, filter: { name: $name}) {
-      info {
-        count
+    query Names ($page: Int!, $name: String!) {
+      characters (page: $page, filter: { name: $name}) {
+        info {
+          count
+        }
+        results {
+          id
+          name
+          image
+        }
       }
-      results {
+    }
+  `
+const ID_QUERY = gql`
+    query Id ($id: ID!){
+      character(id: $id) {
         id
         name
         image
       }
     }
-  }
-`
-const ID_QUERY = gql`
-  query Id ($nameId: ID!){
-	  character(id: $nameId) {
-      id
-  	  name
-      image
-	  }
-  }
-`
-const EPISODE_QUERY = gql`
-  query Episode ($episodeId: ID!){
-	  episode(id: $episodeId) {
-      characters {
-        id
-        name
-        image
-      }
-	  }
-  }
   `
+const EPISODE_QUERY = gql`
+    query Episode ($episodeId: ID!){
+      episode(id: $episodeId) {
+        characters {
+          id
+          name
+          image
+        }
+      }
+    }
+  `
+const favourites = reactive([0])
+favourites.pop()
+
+export function getFavourites() {
+  return favourites
+}
+
+function addFavourite(id: number) {
+  favourites.push(id)
+}
+
+export function deleteFavourite(id: number) {
+  favourites.splice(favourites.indexOf(id), 1)
+}
+
 export default defineComponent({
+
   name: 'HomeView',
+
   components: {
     SearchBar
   },
+
   data() {
     return {
       page: 1,
       input: '',
-      id: {name: '', nameId: 1, episode: 1},
-      buttonClicked: { name: false, id: false, episode: false },
+      id: { name: '', nameId: 1, episode: 1 },
+      buttonClicked: { name: true, id: false, episode: false },
     }
   },
 
   setup() {
+
     const variables = reactive({
-      nameId: 1,
+      id: 1,
       episodeId: 1,
       page: 1,
       name: ''
     })
-
-    const favourites = reactive([0])
-    favourites.pop()
 
     const { result: resultNames, loading: loadingNames, error: errorNames } = useQuery(NAMES_QUERY, variables)
     const { result: resultId, loading: loadingId, error: errorId } = useQuery(ID_QUERY, variables)
@@ -118,27 +140,14 @@ export default defineComponent({
       variables.name = name
     }
 
-    function setNameId(id: number) {
+    function setId(id: number) {
       if (isNaN(id)) return
-      variables.nameId = id
+      variables.id = id
     }
 
     function setEpisodeId(id: number) {
       if (isNaN(id)) return
       variables.episodeId = id
-    }
-
-    function addFavourite(id: number) {
-      if (favourites.indexOf(id) !== -1) return
-      favourites.push(id)
-    }
-
-    function deleteFavourite(id: number) {
-      favourites.splice(id, 1)
-    }
-
-    function getFavourites() {
-      return favourites
     }
 
     return {
@@ -147,7 +156,7 @@ export default defineComponent({
       errorNames: errorNames, errorId: errorId, errorEpisode: errorEpisode,
       setPage,
       setName,
-      setNameId,
+      setId: setId,
       setEpisodeId,
       addFavourite,
       deleteFavourite,
@@ -166,6 +175,6 @@ export default defineComponent({
       this.page = page
     }
   }
-
 });
+
 </script>
